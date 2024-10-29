cf1c1caf1ada35fb41091bce34e65eaaabdcb786
1GabsFps <gabriel.neco@live.com>
2024-10-29 14:41:24 -0300

    alterações na pagina de pagamento (ainda nao funcional)

--- src/pages/Pagar/Pagamento.js
+++ src/pages/Pagar/Pagamento.js
@@ -8,12 +8,23 @@ import { GlobalStyle, MainContainer, HeaderContainer, FormPagamento, Image, Titl
 
 export default function Pagamento() {
     const [quantia, setQuantia] = useState("");
-    const [preferenceId, setPreferenceId] = useState(null);
+    const [preferenceId, setPreferenceId] = useState("");
+    const [paymentStatus, setPaymentStatus] = useState(null);
+    const token = "TEST-5462818721115165-090312-c154f5048e76cc504e0d051206edb322-236803241";
 
     useEffect(() => {
         const script = document.createElement("script");
         script.src = "https://sdk.mercadopago.com/js/v2";
         script.async = true;
+
+        script.onload = () => {
+            console.log("MercadoPago script carregado com sucesso");
+        };
+
+        script.onerror = () => {
+            console.error("Erro ao carregar o script do MercadoPago");
+        };
+
         document.body.appendChild(script);
 
         return () => {
@@ -24,7 +35,7 @@ export default function Pagamento() {
     }, []);
 
     const handlePagamento = async (event) => {
-        event.preventDefault(); // Prevent the default form submission
+        event.preventDefault(); // Evita o envio padrão do formulário
 
         try {
             const preference = {
@@ -41,37 +52,127 @@ export default function Pagamento() {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
-                    "Authorization": `Bearer TEST-5462818721115165-090312-c154f5048e76cc504e0d051206edb322-236803241`
+                    "Authorization": `Bearer ${token}`
                 },
                 body: JSON.stringify(preference)
             });
 
+            if (!response.ok) {
+                const errorData = await response.json();
+                throw new Error(`Erro HTTP! status: ${response.status}, mensagem: ${errorData.message}`);
+            }
+
             const data = await response.json();
             setPreferenceId(data.id);
 
-            const mp = new window.MercadoPago("TEST-180d8563-7589-4864-8447-bde13096c8ef", {
-                locale: "pt-BR"
-            });
+            const iframe = document.createElement("iframe");
+            iframe.src = `https://mercadopago.com.br/checkout/v1/modal?preference-id=${data.id}&from-widget=true`;
+            iframe.width = "100%";
+            iframe.height = "100%";
+            iframe.style = "z-index:1;display:block;position:fixed;left:0;top:0;";
+            iframe.frameBorder = "0";
+            iframe.transition = "height 2s ease";
 
-            mp.checkout({
-                preference: {
-                    id: data.id
-                },
-                autoOpen: true
-            });
+            const closeButton = document.createElement("button");
+            closeButton.innerText = "Fechar";
+            closeButton.style = `
+                position: fixed;
+                top: 10px;
+                right: 10px;
+                z-index: 2;
+                background-color: #007bff;
+                color: white;
+                border: none;
+                padding: 10px 20px;
+                cursor: pointer;
+                border-radius: 5px;
+                font-size: 16px;
+                box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+                transition: background-color 0.3s ease;
+            `;
 
-            // Listen for the payment result
-            window.addEventListener("message", async (event) => {
-                if (event.data.type === "checkout_closed") {
-                    const paymentStatus = event.data.result.status;
-                    const paymentAmount = quantia;
+            closeButton.onmouseover = () => {
+                closeButton.style.backgroundColor = "#0056b3";
+            };
 
-                    // print payment status, amount, and card ID
-                    console.log("Payment status:", paymentStatus);
-                    console.log("Payment amount:", paymentAmount);
-
+            closeButton.onmouseout = () => {
+                closeButton.style.backgroundColor = "#007bff";
+            };
+            closeButton.onclick = () => {
+                if (document.body.contains(iframe)) {
+                    document.body.removeChild(iframe);
                 }
-            });
+                if (document.body.contains(closeButton)) {
+                    document.body.removeChild(closeButton);
+                }
+            };
+
+            document.body.appendChild(iframe);
+            document.body.appendChild(closeButton);
+
+            const handleVisibilityChange = async () => {
+                if (document.visibilityState === "visible") {
+                    try {
+                        console.log("Verificando status do pagamento...");
+                        const statusResponse = await fetch(`https://api.mercadopago.com/checkout/preferences/${preferenceId}`, {
+                            headers: {
+                                "Content-Type": "application/json",
+                                "Authorization": `Bearer ${token}`
+                            }
+                        });
+
+                        if (!statusResponse.ok) {
+                            throw new Error(`Erro HTTP! status: ${statusResponse.status}`);
+                        }
+
+                        const statusData = await statusResponse.json();
+                        const paymentAmount = statusData.items[0].unit_price;
+                        const fetchedPaymentStatus = statusData.status;
+
+                        console.log(`Status do pagamento: ${fetchedPaymentStatus}`);
+                        setPaymentStatus(fetchedPaymentStatus);
+
+                        if (fetchedPaymentStatus === "approved") {
+                            console.log("Transação aprovada, enviando dados para a API...");
+                            const apiResponse = await fetch("http://127.0.0.1:8000/recarregar/", {
+                                method: "POST",
+                                headers: {
+                                    "Content-Type": "application/json"
+                                },
+                                body: JSON.stringify({
+                                    valor: paymentAmount,
+                                    status: fetchedPaymentStatus,
+                                    token: document.cookie.split("=")[1],
+                                    cartao_id: localStorage.getItem("cartao_id")
+                                }),
+                                mode: 'cors'
+                            });
+
+                            if (!apiResponse.ok) {
+                                throw new Error(`Erro HTTP! status: ${apiResponse.status}`);
+                            }
+
+                            const apiData = await apiResponse.json();
+                            console.log("Resposta da API:", apiData);
+                        } else {
+                            console.log("Transação não aprovada");
+                        }
+                    } catch (apiError) {
+                        console.error("Erro ao enviar dados para a API:", apiError);
+                    } finally {
+                        // Fechar o iframe após verificar o status
+                        if (document.body.contains(iframe)) {
+                            document.body.removeChild(iframe);
+                        }
+                        if (document.body.contains(closeButton)) {
+                            document.body.removeChild(closeButton);
+                        }
+                    }
+                }
+            };
+
+            document.addEventListener("visibilitychange", handleVisibilityChange);
+
         } catch (error) {
             console.error("Error processing payment:", error);
         }
@@ -95,6 +196,11 @@ export default function Pagamento() {
                         <Image src={Flogo} alt="Flogo" />
                     </div>
                 </FormPagamento>
+                {paymentStatus && (
+                    <div>
+                        <p>Status do pagamento: {paymentStatus}</p>
+                    </div>
+                )}
             </MainContainer>
         </div>
     );
